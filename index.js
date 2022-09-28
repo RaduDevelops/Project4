4801d90e16d0866bc9027e6907603474c32961d1
RaduDevelops <raducu_hogas@yahoo.com>
2022-09-28 20:55:21 +0300

    ultimul commit pe 28.09

--- index.js
+++ index.js
@@ -4,25 +4,21 @@ let textInput = document.querySelector("input");
 let pcontainer = document.getElementsByClassName("p-container");
 
 function adding(){
-    const taskContainer = document.getElementsByClassName("tasks");
     const parent = document.createElement('div');
     parent.classList.add("task");
+    
 
     const child1 = document.createElement('div');
     child1.classList.add("unchecked");
     child1.innerHTML = textInput.value;
 
     const child2 = document.createElement('button');
-    const child22 =document.createElementNS("http://www.w3.org/2000/svg","svg");
+    const child22 = document.createElementNS("http://www.w3.org/2000/svg","svg");
     child2.appendChild(child22);
     child2.classList.add("inactive");
 
     parent.appendChild(child1);
-  
-
-
-    document.body.pcontainer.taskContainer.appendChild(parent);
-    
+    taskContainer.append(parent);
     };
 
 adder.addEventListener("click",(e)=>{
