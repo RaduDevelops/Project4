96011a3e181c1b7746f901af99a18510dfd669b3
RaduDevelops <raducu_hogas@yahoo.com>
2022-09-30 00:36:26 +0300

    commit pe 30.09

--- index.js
+++ index.js
@@ -1,26 +1,52 @@
+//Declaring the vars
 let adder = document.getElementById("adder");
 const taskContainer = document.getElementById("my-tasks");
 let textInput = document.querySelector("input");
 let pcontainer = document.getElementsByClassName("p-container");
 
+
+// The function that adds a task
 function adding(){
     const parent = document.createElement('div');
     parent.classList.add("task");
-    
-
+ // Creating the todo
     const child1 = document.createElement('div');
     child1.classList.add("unchecked");
     child1.innerHTML = textInput.value;
-
+// Creating the 'checking' button
     const child2 = document.createElement('button');
-    const child22 = document.createElementNS("http://www.w3.org/2000/svg","svg");
-    child2.appendChild(child22);
     child2.classList.add("inactive");
-
+    const child22 = document.createElement("i");
+    child22.classList.add("fa-solid","fa-check");
+    child2.appendChild(child22);
+//Creating the 'remove' button
+    const child3 = document.createElement('button');
+    child3.classList.add("remove-btn");
+    const child33 = document.createElement("i");
+    child33.classList.add("fa-solid","fa-trash");
+    child3.appendChild(child33);
+//appending the children to the parent div, respectively to the task container
     parent.appendChild(child1);
+    parent.appendChild(child2);
+    parent.appendChild(child3);
     taskContainer.append(parent);
     };
 
+
+// The function that removes a task
+    function deleteTask(e){
+      const item = e.target;
+     if(item.classList[1] === "fa-trash"){
+      const todo= item.parentElement.parentElement;
+      todo.remove();
+      }
+      //  REMAIN HERE !!!!
+      if(item.classList[1] === "fa-check"){
+      }
+    }
+
+
+// Linking the adding function to the "Add Task" button
 adder.addEventListener("click",(e)=>{
     e.preventDefault();
   if(textInput.value!==""){
@@ -30,6 +56,17 @@ adder.addEventListener("click",(e)=>{
     console.log("not added");
     alert("Please write a task before addind it!");
   }
-})
+});
 
-console.log(taskContainer);
+taskContainer.addEventListener("click", deleteTask)
+
+
+
+
+
+/* Linking the delete function to the "remove" button button
+ removeButton.addEventListener("click",(e)=>{
+  e.preventDefault();
+  deleteTask();
+ }) */
+
